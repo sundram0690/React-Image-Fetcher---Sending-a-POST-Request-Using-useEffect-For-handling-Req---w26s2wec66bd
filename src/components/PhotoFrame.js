6ef682b99d2e33c67b42f7a09718a6a65456312c
sundram0690 <73088863+sundram0690@users.noreply.github.com>
2023-12-20 08:03:49 +0530

    Removed Warning

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../styles/App.css'
 export const PhotoFrame = (props) => {
 
-    if(props.url == undefined){
+    if(props.url === undefined){
         return <></>
     }else{
         return <div className="photoframe">
-            <img src={props.url} />
+            <img src={props.url} alt=""/>
             <div className="caption">{props.title}</div>
         </div>
     }
